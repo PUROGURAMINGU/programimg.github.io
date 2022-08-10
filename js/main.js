9924b45a9b6acc90e55e4b0a328f30f69c83d955
PUROGURAMINGU <110904658+PUROGURAMINGU@users.noreply.github.com>
2022-08-10 17:34:30 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,69 @@
+'use strict';
+
+{
+  const open = document.getElementById('open');
+  const close = document.getElementById('close');
+  const overlay = document.querySelector('.overlay');
+  const images = document.querySelectorAll('.pic img');
+  const btn = document.querySelector('.btn');
+  const toTop = document.getElementById('to_top');
+  const onScrollObserver = new IntersectionObserver(onScrollCallback);
+  
+  let currentIndex = 0;
+  function play() {
+    setTimeout(() => {
+      images[currentIndex].classList.remove('current');
+      currentIndex++;
+      if (currentIndex > images.length - 1) {
+        currentIndex = 0;
+      }
+      images[currentIndex].classList.add('current');
+      play();
+    }, 4000);
+  }
+
+  function onScrollCallback(entries) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        toTop.classList.add('scrolled');
+      } else {
+        toTop.classList.remove('scrolled');
+      }
+    });
+  }
+
+  onScrollObserver.observe(document.getElementById('target'));
+  
+  play();
+  
+  toTop.addEventListener('click', e => {
+    e.preventDefault();
+
+    window.scrollTo({
+      top: 0,
+      behavior:'smooth',
+    });
+  });
+
+  open.addEventListener('click', () => {
+    overlay.classList.add('show');
+    open.classList.add('hide');
+  });
+
+  close.addEventListener('click', () => {
+    overlay.classList.remove('show');
+    open.classList.remove('hide');
+  });
+
+  function process() {
+    document.getElementById('trigger').classList.toggle('changed');
+  }
+  
+  document.getElementById('trigger').addEventListener('click', process);
+  
+  btn.addEventListener('click', () => {
+    btn.classList.toggle('appper');
+  });
+}
+
+
