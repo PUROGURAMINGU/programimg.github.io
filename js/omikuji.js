78cfa9050be911b40ad26ebbfe841c342f8e5bf9
PUROGURAMINGU <110904658+PUROGURAMINGU@users.noreply.github.com>
2022-08-31 16:24:59 +0900

    Add files via upload

--- js/omikuji.js
+++ js/omikuji.js
@@ -1,7 +1,59 @@
 'use strict';
 {
   const bttn = document.getElementById('bttn');
+  const open = document.getElementById('open');
+  const close = document.getElementById('close');
+  const overlay = document.querySelector('.overlay');
+  const images = document.querySelectorAll('.pic img');
+  const toTop = document.getElementById('to_top');
+  const onScrollObserver = new IntersectionObserver(onScrollCallback);
+  let currentIndex = 0;
+
+  function play() {
+    setTimeout(() => {
+      images[currentIndex].classList.remove('current');
+      currentIndex++;
+      if (currentIndex > images.length - 1) {
+        currentIndex = 0;
+      }
+      images[currentIndex].classList.add('current');
+      play();
+    }, 4000);
+  }
+
+  function onScrollCallback(entries) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        toTop.classList.add('scrolled');
+      } else {
+        toTop.classList.remove('scrolled');
+      }
+    });
+  }
+
+  onScrollObserver.observe(document.getElementById('target'));
   
+  play();
+
+  toTop.addEventListener('click', e => {
+    e.preventDefault();
+
+    window.scrollTo({
+      top: 0,
+      behavior:'smooth',
+    });
+  });
+
+  open.addEventListener('click', () => {
+    overlay.classList.add('show');
+    open.classList.add('hide');
+  });
+
+  close.addEventListener('click', () => {
+    overlay.classList.remove('show');
+    open.classList.remove('hide');
+  });
+
   bttn.addEventListener('click', () => {
     const n = Math.random();
     if (n < 0.05) {
